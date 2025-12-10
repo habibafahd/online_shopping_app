f67f897872048fc76f8099d4c1da90a98ee33f6f
Habiba <habibafahd66@gmail.com>
2025-12-10 14:01:19 +0200

    Before adding ranna

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'login_screen.dart';
-
-// Manually added Firebase configuration for web
-const FirebaseOptions firebaseOptions = FirebaseOptions(
-  apiKey: "AIzaSyAMj4hBG08a2_OIR6dWtII22GOXmZKIW-8",
-  authDomain: "online-shopping-app-7595d.firebaseapp.com",
-  projectId: "online-shopping-app-7595d",
-  storageBucket: "online-shopping-app-7595d.appspot.com",
-  messagingSenderId: "561760250720",
-  appId: "1:561760250720:web:70ddf91960f1c808fa8c05",
-);
+import 'home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: firebaseOptions);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Check if a user is already logged in
+    final User? user = FirebaseAuth.instance.currentUser;
+
     return MaterialApp(
       title: 'Online Shopping App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(), // Launch LoginScreen first
-      debugShowCheckedModeBanner: false, // <-- removes the debug banner
+      // If user is logged in → go straight to HomeScreen
+      // If not → show LoginScreen
+      home: user == null ? LoginScreen() : HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
